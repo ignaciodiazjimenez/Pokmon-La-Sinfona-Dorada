a6d5c5f6dbcb1d82675765d1bf87d2d93f3f5ca6
Javier <j.arrabal8719@gmail.com>
2024-03-26 23:52:19 +0100

    Añadido sitio de WordPress al repositorio

---
+++ wordpress/wp-content/themes/x-t9/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '9d904538fe76779b0c30e405de7c0c26901772fa',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '9d904538fe76779b0c30e405de7c0c26901772fa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vektor-inc/tgm-plugin-activation' => array(
+            'pretty_version' => '2.6.2',
+            'version' => '2.6.2.0',
+            'reference' => 'ae20a712c7bbd67f1bef0af9a3f5f07359a94d48',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../vektor-inc/tgm-plugin-activation',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
